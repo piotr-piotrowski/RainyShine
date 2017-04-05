3517dd53355be2eaaf7694b5d1f1fea8690fbb7b
Piotr Piotrowski <piotr@piotrovski.com>
2017-04-05 14:30:56 +0200

    Lecture 90. iOS 10 App: Rainy Shine - Creating the interface with IBOutlets

---
+++ rainyshiny/WeatherVC.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherVC.swift
+//  rainyshiny
+//
+//  Created by Piotr Piotrowski on 04/04/2017.
+//  Copyright © 2017 Piotr Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var temperatureLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherTypeLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
