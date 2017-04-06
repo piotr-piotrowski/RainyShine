5681a3a1028dc0c98e2222c7db5f57a7114d48dc
Piotr Piotrowski <piotr@piotrovski.com>
2017-04-06 14:37:51 +0200

    Lecture 95. iOS 10 App: Rainy Shine - Parse JSON data

--- rainyshiny/CurrentWeather.swift
+++ rainyshiny/CurrentWeather.swift
@@ -54,7 +54,31 @@ class CurrentWeather {
         let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)
         Alamofire.request(currentWeatherURL!).responseJSON { response in
             let result = response.result
-            print(response)
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    print(self._cityName)
+                }
+                
+                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String, AnyObject> {
+                    if let currentTemperature = main["temp"] as? Double {
+                        let kelvinToFahrenheitPreDivision = (currentTemperature * (9/5) - 459.67)
+                        let kelvinToFahrenheit = Double(round(10 * kelvinToFahrenheitPreDivision/10))
+                        self._currentTemp = kelvinToFahrenheit
+                        print(self._currentTemp)
+                    }
+                 }
+                
+                
+            }
         }
         completed()
     }
